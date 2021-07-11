5b84c419856cb87f37a6307c5932e8ccdf0c928a
James Rissler <risslerjames@gmail.com>
2021-07-11 07:52:43 -0500

    init commit

---
+++ charts_live/lib/charts_live.ex
@@ -0,0 +1,18 @@
+defmodule ChartsLive do
+  @moduledoc """
+  Documentation for `ChartsLive`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChartsLive.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ charts_live/test/charts_live_test.exs
@@ -0,0 +1,8 @@
+defmodule ChartsLiveTest do
+  use ExUnit.Case
+  doctest ChartsLive
+
+  test "greets the world" do
+    assert ChartsLive.hello() == :world
+  end
+end
